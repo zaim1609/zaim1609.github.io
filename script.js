493773678fd37b368711a38d92f63dc24e37be80
zaim1609 <54833824+zaim1609@users.noreply.github.com>
2021-06-18 17:00:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+alert("wa");
+alert("ana minta maaf ye");
+alert("klo kemaren agak ngebentak nadany");
+alert("jujur gw kemaren lagi campur aduk perasaannya");
+alert("soalnya lo ilang gk ad kabar");
+alert("dan waktu itu aku ada masalah lain juga");
+
+var tes = confirm("dimaapin gk nih");
+if(tes == true){
+    alert("maacih ya");
+}else{
+    alert("yah baru mo dikasi mobil");
+}
+$(document).redy(function(){
+
+      $('#gambar').show();
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
